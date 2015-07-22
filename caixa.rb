4e8bc74eac95dc78c9ee52d5c0fb538ce556b95d
Leandro <ldesouzarodrigues@yahoo.com.br>
2015-07-22 14:54:18 -0300

    easy way

--- caixa.rb
+++ caixa.rb
@@ -2,14 +2,20 @@ class Caixa
 
   def sacar(valor)
     valor = valor
+
+    nota10 = 10
+    nota20 = 20
+    nota50 = 50
+    nota100 = 100
+
     if valor == 10
-      saida = '1 nota de 10 reais'
+      saida = '1 nota de #{nota10} reais'
     elsif valor == 20
-      saida = '1 nota de 20 reais'
+      saida = '1 nota de #{nota20} reais'
     elsif valor == 30
-      saida = '1 nota de 20 reais e 1 nota de 10 reais'
+      saida = '1 nota de #{nota20 e uma nota de #{nota10} reais'
     elsif valor == 50
-      saida = '1 nota de 50 reais'
+      saida = '1 nota de #{nota50} reais'
     end
     saida
   end
