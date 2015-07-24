73e9e8017a129beb3e6221a7db8e9fa376da46dc
Leandro <ldesouzarodrigues@yahoo.com.br>
2015-07-24 16:16:03 -0300

    in progress...

--- caixa2.rb
+++ caixa2.rb
@@ -1,8 +1,8 @@
 class Caixa
+  attr_reader :q
 
   def sacar(valor)
   	@valor = valor
-    saida = ''
     @q = { quantidade_100: 0,
            quantidade_50:  0,
            quantidade_20:  0,
@@ -13,33 +13,35 @@ class Caixa
     contar_notas_de(20)
     contar_notas_de(10)
 
-    if q[:quantidade_100] != 0
-      saida += "#{q[:quantidade_100]} nota de 100 reais"
-    end
+    # if q[:quantidade_100] != 0
+    #   saida += "#{q[:quantidade_100]} nota de 100 reais"
+    # end
 
-    if q[:quantidade_50] != 0 && q[:quantidade_100] != 0
-      saida += " e #{q[:quantidade_50]} nota de 50 reais"
-    elsif q[:quantidade_50] != 0
-      saida += "#{q[:quantidade_50]} nota de 50 reais"
-    end
+    # if q[:quantidade_50] != 0 && q[:quantidade_100] != 0
+    #   saida += " e #{q[:quantidade_50]} nota de 50 reais"
+    # elsif q[:quantidade_50] != 0
+    #   saida += "#{q[:quantidade_50]} nota de 50 reais"
+    # end
 
-    if q[:quantidade_20] != 0 && q[:quantidade_50] != 0 && q[:quantidade_100] != 0
-      saida += " e #{q[:quantidade_20]} nota de 20 reais"
-    elsif q[:quantidade_20] != 0
-      saida += "#{q[:quantidade_20]} nota de 20 reais"
-    end
+    # if q[:quantidade_20] != 0 && q[:quantidade_50] != 0 && q[:quantidade_100] != 0
+    #   saida += " e #{q[:quantidade_20]} nota de 20 reais"
+    # elsif q[:quantidade_20] != 0
+    #   saida += "#{q[:quantidade_20]} nota de 20 reais"
+    # end
 
-    if q[:quantidade_10] != 0 && q[:quantidade_20] != 0 && q[:quantidade_50] != 0 && q[:quantidade_100] != 0
-      saida += " e #{:q[quantidade_10]} nota de 10 reais"
-    elsif q[:quantidade_10] != 0
-      saida += "#{q[:quantidade_10]} nota de 10 reais"
-    end
+    # if q[:quantidade_10] != 0 && q[:quantidade_20] != 0 && q[:quantidade_50] != 0 && q[:quantidade_100] != 0
+    #   saida += " e #{:q[quantidade_10]} nota de 10 reais"
+    # elsif q[:quantidade_10] != 0
+    #   saida += "#{q[:quantidade_10]} nota de 10 reais"
+    # end
   end
 
-  private
+  # private
 
   def contar_notas_de(valor_nota)
-    @valor -= @valor / valor_nota
-    @q[:"quantidade_#{valor_nota}"] += @valor / valor_nota
+    @valor -= valor_nota
+    #quantidade -= @valor / valor_nota
+
+    # @q[:"quantidade_#{valor_nota}"] += @valor / valor_nota
   end
 end
--- caixa_spec.rb
+++ caixa_spec.rb
@@ -1,42 +1,50 @@
 require 'rspec'
-require './caixa.rb'
+require './caixa2.rb'
 
 RSpec.describe Caixa do
   describe '#sacar' do
     let(:caixa) { Caixa.new }
 
-    context 'quando passo 10 reais' do
-      it 'retorna notas' do
-        valor = caixa.sacar(10)
-        expect(valor).to eql('1 nota de 10 reais')
-      end
-    end
+    # context 'quando passo 10 reais' do
+    #   it 'retorna notas' do
+    #     valor = caixa.sacar(10)
+    #     expect(valor).to eql('1 nota de 10 reais')
+    #   end
+    # end
 
-    context 'quando passo 20 reais' do
-      it 'retorna notas' do
-        valor = caixa.sacar(20)
-        expect(valor).to eql('1 nota de 20 reais')
-      end
-    end
+    # context 'quando passo 20 reais' do
+    #   it 'retorna notas' do
+    #     valor = caixa.sacar(20)
+    #     expect(valor).to eql('1 nota de 20 reais')
+    #   end
+    # end
 
-    context 'quando passo 30 reais' do
-      it 'retorna notas' do
-        valor = caixa.sacar(30)
-        expect(valor).to eql('1 nota de 20 reais e 1 nota de 10 reais')
-      end
-    end
+    # context 'quando passo 30 reais' do
+    #   it 'retorna notas' do
+    #     valor = caixa.sacar(30)
+    #     expect(valor).to eql('1 nota de 20 reais e 1 nota de 10 reais')
+    #   end
+    # end
 
-    context 'quando passo 50 reais' do
-      it 'retorna notas' do
-        valor = caixa.sacar(50)
-        expect(valor).to eql('1 nota de 50 reais')
-      end
-    end
+    # context 'quando passo 50 reais' do
+    #   it 'retorna notas' do
+    #     valor = caixa.sacar(50)
+    #     expect(valor).to eql('1 nota de 50 reais')
+    #   end
+    # end
 
-    context 'when try to withdraw 430 R$' do
-      it 'complete the withdrawal operation' do
-        valor = caixa.sacar(430)
-        expect(valor).to eql('4 nota de 100 reais e 1 nota de 20 reais e 1 nota de 10 reais')
+    # context 'when try to withdraw 430 R$' do
+    #   it 'complete the withdrawal operation' do
+    #     valor = caixa.sacar(430)
+    #     expect(valor).to eql('4 nota de 100 reais e 1 nota de 20 reais e 1 nota de 10 reais')
+    #   end
+    # end
+
+    context 'teste' do
+      it 'teste' do
+        caixa.sacar(300)
+        valor = caixa.contar_notas_de(100)
+        caixa.q.should include(:quantidade_100 => 3)
       end
     end
   end
